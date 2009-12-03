248172c17ff8ac4d40c8726b4be08675455c1e73
Robert Nelson <robertcnelson@gmail.com>
2009-12-03 12:07:13 -0600

    merge 2.6.31.6-x6.0 from 2.6-dev

--- sync_angstrom_patches.sh
+++ sync_angstrom_patches.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 DIR=$PWD
+mkdir -p ${DIR}/patches/angstrom
 cd ~/git_repo/openembedded/
 git pull
-cp -rv ~/git_repo/openembedded/recipes/linux/linux-omap-2.6.29/* ${DIR}/angstrom
+git checkout origin/org.openembedded.dev -b upstream-copy
+cp -rv ~/git_repo/openembedded/recipes/linux/linux-omap-2.6.31/* ${DIR}/patches/angstrom
+cp -rv ~/git_repo/openembedded/recipes/linux/linux-omap_2.6.31.bb ${DIR}/patches/angstrom
+cp -rv ~/git_repo/openembedded/recipes/powervr-drivers/omap3-sgx-modules/* ${DIR}/sgx
+git checkout origin/org.openembedded.dev -f
+git branch upstream-copy -D
 cd ${DIR}/
-bzr add angstrom/
+bzr add ${DIR}/patches/angstrom
+bzr add ${DIR}/sgx
 bzr diff > check.diff
-gedit check.diff build_kernel.sh &
+gedit check.diff *.sh ${DIR}/patches/angstrom/linux-omap_2.6.31.bb &
 
 
