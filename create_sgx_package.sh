e9a7293d46a0f61edd2115d2486e433e1b2f3663
Robert Nelson <robertcnelson@gmail.com>
2010-05-31 09:34:47 -0500

    sgx: clean up install-SGX.sh and run-SGX.sh and remove deploy

--- create_sgx_package.sh
+++ create_sgx_package.sh
@@ -22,9 +22,10 @@ SGX_BIN=OMAP35x_Graphics_SDK_setuplinux_${SGX_VERSION}.bin
 
 sudo rm -rfd ${DIR}/SDK/ || true
 mkdir -p ${DIR}/SDK/
+mkdir -p ${DIR}/dl/
 
 function sgx_setup {
-if [ -e ${DIR}/dl/${SGX_BIN} ]; then
+if [ -e ${DIR}/${SGX_BIN} ]; then
   echo "${SGX_BIN} found"
   if [ -e  ${DIR}/dl/OMAP35x_Graphics_SDK_setuplinux_${SGX_VERSION}/Makefile ]; then
     echo "Extracted ${SGX_BIN} found"
@@ -32,9 +33,9 @@ if [ -e ${DIR}/dl/${SGX_BIN} ]; then
   else
     cd ${DIR}/dl/
     echo "${SGX_BIN} needs to be executable"
-    sudo chmod +x ./${SGX_BIN}
+    sudo chmod +x ${DIR}/${SGX_BIN}
     echo "running ${SGX_BIN}, accept all defaults and agree to the license"
-    ./${SGX_BIN} --mode console --prefix ${DIR}/dl/OMAP35x_Graphics_SDK_setuplinux_${SGX_VERSION}
+    ${DIR}/${SGX_BIN} --mode console --prefix ${DIR}/dl/OMAP35x_Graphics_SDK_setuplinux_${SGX_VERSION}
     cd ${DIR}
     SGX+=E
   fi
@@ -43,86 +44,90 @@ else
   echo "${SGX_BIN} not found"
   echo "Download Latest from"
   echo "DL From: http://software-dl.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/gfxsdk/latest/index_FDS.html" 
-  echo "Copy to: ${DIR}/dl"
+  echo "Copy to: ${DIR}"
   echo ""
 fi
 }
 
 function file-install-SGX {
 
-echo "#!/bin/bash" > ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo "DIR=\$PWD" >>  ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo "if [ \$(uname -m) == \"armv7l\" ] ; then" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo " if [ -e  \${DIR}/target_libs.tar.gz ]; then" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo "  sudo ln -sf /usr/lib/libXdmcp.so.6.0.0 /usr/lib/libXdmcp.so.0" >> ${DIR}/SDK/install-SGX.sh
-echo "  sudo ln -sf /usr/lib/libXau.so.6.0.0 /usr/lib/libXau.so.0" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo "  echo \"Extracting target files to rootfs\"" >> ${DIR}/SDK/install-SGX.sh
-echo "  sudo tar xf target_libs.tar.gz -C /" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo "  if which lsb_release >/dev/null 2>&1 && [ \"\`lsb_release -is\`\" = Ubuntu ]; then" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo "    if [ \$(lsb_release -sc) == \"jaunty\" ]; then" >> ${DIR}/SDK/install-SGX.sh
-echo "      sudo cp /opt/pvr/pvr /etc/rcS.d/S60pvr.sh" >> ${DIR}/SDK/install-SGX.sh
-echo "      sudo chmod +x /etc/rcS.d/S60pvr.sh" >> ${DIR}/SDK/install-SGX.sh
-echo "    else" >> ${DIR}/SDK/install-SGX.sh
-echo "      #karmic/lucid/etc" >> ${DIR}/SDK/install-SGX.sh
-echo "      sudo cp /opt/pvr/pvr /etc/init.d/pvr" >> ${DIR}/SDK/install-SGX.sh
-echo "      sudo chmod +x /etc/init.d/pvr" >> ${DIR}/SDK/install-SGX.sh
-echo "      sudo update-rc.d pvr defaults" >> ${DIR}/SDK/install-SGX.sh
-echo "    fi" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo "  else" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo "    sudo cp /opt/pvr/pvr /etc/init.d/pvr" >> ${DIR}/SDK/install-SGX.sh
-echo "    sudo chmod +x /etc/init.d/pvr" >> ${DIR}/SDK/install-SGX.sh
-echo "    sudo update-rc.d pvr defaults" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo "  fi" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo " else" >> ${DIR}/SDK/install-SGX.sh
-echo "  echo \"target_libs.tar.gz is missing\"" >> ${DIR}/SDK/install-SGX.sh
-echo "  exit" >> ${DIR}/SDK/install-SGX.sh
-echo " fi" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
-echo "else" >> ${DIR}/SDK/install-SGX.sh
-echo " echo \"This script is to be run on an armv7 platform\"" >> ${DIR}/SDK/install-SGX.sh
-echo " exit" >> ${DIR}/SDK/install-SGX.sh
-echo "fi" >> ${DIR}/SDK/install-SGX.sh
-echo "" >> ${DIR}/SDK/install-SGX.sh
+cat > ${DIR}/SDK/install-SGX.sh <<installSGX
+#!/bin/bash
+
+DIR=\$PWD
+
+if [ \$(uname -m) == "armv7l" ] ; then
+
+ if [ -e  \${DIR}/target_libs.tar.gz ]; then
+
+  sudo ln -sf /usr/lib/libXdmcp.so.6.0.0 /usr/lib/libXdmcp.so.0
+  sudo ln -sf /usr/lib/libXau.so.6.0.0 /usr/lib/libXau.so.0
+
+  echo "Extracting target files to rootfs"
+  sudo tar xf target_libs.tar.gz -C /
+
+  if which lsb_release >/dev/null 2>&1 && [ "\$(lsb_release -is)" = Ubuntu ]; then
+
+    if [ \$(lsb_release -sc) == "jaunty" ]; then
+      sudo cp /opt/pvr/pvr /etc/rcS.d/S60pvr.sh
+      sudo chmod +x /etc/rcS.d/S60pvr.sh
+    else
+      #karmic/lucid/etc
+      sudo cp /opt/pvr/pvr /etc/init.d/pvr
+      sudo chmod +x /etc/init.d/pvr
+      sudo update-rc.d pvr defaults
+    fi
+
+  else
+
+    sudo cp /opt/pvr/pvr /etc/init.d/pvr
+    sudo chmod +x /etc/init.d/pvr
+    sudo update-rc.d pvr defaults
+
+  fi
+
+ else
+  echo "target_libs.tar.gz is missing"
+  exit
+ fi
+
+else
+ echo "This script is to be run on an armv7 platform"
+ exit
+fi
+
+installSGX
 
 }
 
 function file-run-SGX {
 
-echo "#!/bin/bash" > ${DIR}/SDK/run-SGX.sh
-echo "" >> ${DIR}/SDK/run-SGX.sh
-echo "DIR=\$PWD" >> ${DIR}/SDK/run-SGX.sh
-echo "" >> ${DIR}/SDK/run-SGX.sh
-echo "if [ \$(uname -m) == \"armv7l\" ] ; then" >> ${DIR}/SDK/run-SGX.sh
-echo "" >> ${DIR}/SDK/run-SGX.sh
-echo " sudo rm /etc/powervr-esrev" >> ${DIR}/SDK/run-SGX.sh
-echo " sudo depmod -a omaplfb" >> ${DIR}/SDK/run-SGX.sh
-echo "" >> ${DIR}/SDK/run-SGX.sh
-echo " if which lsb_release >/dev/null 2>&1 && [ \"\`lsb_release -is\`\" = Ubuntu ]; then" >> ${DIR}/SDK/run-SGX.sh
-echo "  if [ \$(lsb_release -sc) == \"jaunty\" ]; then" >> ${DIR}/SDK/run-SGX.sh
-echo "   sudo /etc/rcS.d/S60pvr.sh restart" >> ${DIR}/SDK/run-SGX.sh
-echo "  else " >> ${DIR}/SDK/run-SGX.sh
-echo "   sudo /etc/init.d/pvr restart" >> ${DIR}/SDK/run-SGX.sh
-echo "  fi" >> ${DIR}/SDK/run-SGX.sh
-echo " else" >> ${DIR}/SDK/run-SGX.sh
-echo "  sudo /etc/init.d/pvr restart" >> ${DIR}/SDK/run-SGX.sh
-echo " fi" >> ${DIR}/SDK/run-SGX.sh
-echo "" >> ${DIR}/SDK/run-SGX.sh
-echo "else" >> ${DIR}/SDK/run-SGX.sh
-echo " echo \"This script is to be run on an armv7 platform\"" >> ${DIR}/SDK/run-SGX.sh
-echo " exit" >> ${DIR}/SDK/run-SGX.sh
-echo "fi" >> ${DIR}/SDK/run-SGX.sh
-echo "" >> ${DIR}/SDK/run-SGX.sh
+cat > ${DIR}/SDK/run-SGX.sh <<runSGX
+#!/bin/bash
+
+DIR=\$PWD
+
+if [ \$(uname -m) == "armv7l" ] ; then
+
+ sudo rm /etc/powervr-esrev
+ sudo depmod -a omaplfb
+
+ if which lsb_release >/dev/null 2>&1 && [ "\$(lsb_release -is)" = Ubuntu ]; then
+  if [ \$(lsb_release -sc) == "jaunty" ]; then
+   sudo /etc/rcS.d/S60pvr.sh restart
+  else
+   sudo /etc/init.d/pvr restart
+  fi
+ else
+  sudo /etc/init.d/pvr restart
+ fi
+
+else
+ echo "This script is to be run on an armv7 platform"
+ exit
+fi
+
+runSGX
 
 }
 
@@ -152,11 +157,11 @@ file-run-SGX
 	chmod +x ./SDK/run-SGX.sh
 
 	cd ${DIR}/SDK
-	tar czf ${DIR}/deploy/GFX_${SGX_VERSION}_libs.tar.gz *
+	tar czf ${DIR}/GFX_${SGX_VERSION}_libs.tar.gz *
 	cd ${DIR}
 
 	sudo rm -rfd ${DIR}/SDK/ || true
-	echo "SGX libs are in: deploy/GFX_${SGX_VERSION}_libs.tar.gz"
+	echo "SGX libs are in: GFX_${SGX_VERSION}_libs.tar.gz"
 	
 }
 
@@ -181,8 +186,8 @@ function tar_up_examples {
 	rm -rfd ${DIR}/SDK/GFX_Linux_SDK/OVG
 
 	cd ${DIR}/SDK
-	tar czfv ${DIR}/deploy/GFX_Linux_SDK.tar.gz ./GFX_Linux_SDK
-	echo "SGX examples are in: deploy/GFX_Linux_SDK.tar.gz"
+	tar czfv ${DIR}/GFX_Linux_SDK.tar.gz ./GFX_Linux_SDK
+	echo "SGX examples are in: GFX_Linux_SDK.tar.gz"
 	cd ${DIR}
 }
 
