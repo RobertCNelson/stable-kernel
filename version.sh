451add67fb7ceea3f86ea18b62e4f153c92d7f55
Robert Nelson <voodoo@lvrm>
2010-06-06 11:33:35 -0500

    enable ppp modules as tested by Siddharth Karandikar

--- version.sh
+++ version.sh
@@ -7,7 +7,7 @@ KERNEL_REL=2.6.34
 #STABLE_PATCH=5
 #KERNEL_PATCH=${KERNEL_REL}.${STABLE_PATCH}
 #DL_PATCH=patch-${KERNEL_PATCH}
-ABI=1
+ABI=2
 
 if [ "${IS_LUCID}" ] ; then
 BUILD+=l${ABI}
@@ -15,12 +15,9 @@ else
 BUILD+=x${ABI}
 fi
 
-#USB patches is board specific
-BOARD=beagleboard
-
 BUILDREV=1.0
 DISTRO=cross
 
 export KERNEL_REL BUILD RC_PATCH KERNEL_PATCH
-export BRANCH REL GIT
-export BOARD BUILDREV DISTRO
+export BRANCH REL
+export BUILDREV DISTRO
