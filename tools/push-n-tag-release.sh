b4f92585834fac562483de99bda3a85364b80ebb
Robert Nelson <voodoo@voodoo-e6400>
2010-08-02 14:05:41 -0500

    2.6.34.2-x2 release

---
+++ tools/push-n-tag-release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+#yeah, i'm getting lazy..
+
+DIR=$PWD
+
+if [ -e ${DIR}/version.sh ]; then
+	. version.sh
+
+	if [ "${KERNEL_PATCH}" ] ; then
+		bzr commit -m "${KERNEL_REL}.${STABLE_PATCH}-${BUILD} release"
+		bzr tag "${KERNEL_REL}.${STABLE_PATCH}-${BUILD}"
+		bzr push lp:~beagleboard-kernel/+junk/2.6-stable
+	else
+		bzr commit -m "${KERNEL_REL}-${BUILD} release"
+		bzr tag "${KERNEL_REL}.${STABLE_PATCH}"
+		bzr push lp:~beagleboard-kernel/+junk/2.6-stable
+	fi
+fi
+
