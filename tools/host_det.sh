01b303502a87a68c50129b0730e27a1a48bb70a4
Robert Nelson <robertcnelson@gmail.com>
2010-08-16 11:19:46 -0500

    script change: opensuse support added by Antonio Cavallo

--- tools/host_det.sh
+++ tools/host_det.sh
@@ -1,37 +1,93 @@
 #!/bin/bash -e
 
-unset REDHAT
-unset SUSE
-unset DEBIAN
+#opensuse support added by: Antonio Cavallo
+#https://launchpad.net/~a.cavallo
+
+function warning { echo "! $@" >&2; }
+function error { echo "* $@" >&2; exit 1; }
+function info { echo "+ $@" >&2; }
+function ltrim { echo "$1" | awk '{ gsub(/^[ \t]+/,"", $0); print $0}'; }
+function rtrim { echo "$1" | awk '{ gsub(/[ \t]+$/,"", $0); print $0}'; }
+function trim { local x="$( ltrim "$1")"; x="$( rtrim "$x")"; echo "$x"; }
+
 
 
 function detect_host {
+local REV DIST PSEUDONAME
 
 if [ -f /etc/redhat-release ] ; then
 	DIST='RedHat'
-	PSUEDONAME=$(cat /etc/redhat-release | sed s/.*\(// | sed s/\)//)
+	PSEUDONAME=$(cat /etc/redhat-release | sed s/.*\(// | sed s/\)//)
 	REV=$(cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//)
-	REDHAT=1
+    echo "redhat-$REV"
 elif [ -f /etc/SuSE-release ] ; then
 	DIST=$(cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//)
 	REV=$(cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //)
-	SUSE=1
+    trim "suse-$REV"
 elif [ -f /etc/debian_version ] ; then
 	DIST="Debian Based"
 	REV=""
-	DEBIAN=1
+    echo "debian-$REV"
 fi
 
 }
 
 function redhat_reqs
 {
-	echo "Not implemented yet"
+	echo "RH Not implemented yet"
 }
 
 function suse_regs
 {
-	echo "Not implemented yet"
+    local BUILD_HOST="$1"   
+# --- SuSE-release ---
+    if [ ! -f /etc/SuSE-release ]
+    then
+        cat >&2 <<@@
+Missing /etc/SuSE-release file
+ this file is part of the efault suse system. If this is a
+ suse system for real, please install the package with:
+    
+    zypper install openSUSE-release   
+@@
+        return 1
+    fi
+
+
+# --- patch ---
+    if [ ! $( which patch ) ]
+    then
+        cat >&2 <<@@
+Missing patch command,
+ it is part of the opensuse $BUILD_HOST distribution so it can be 
+ installed simply using:
+
+    zypper install patch
+
+@@
+        return 1
+    fi
+
+# --- mkimage ---
+    if [ ! $( which mkimage ) ]
+    then
+        cat >&2 <<@@
+Missing mkimage command.
+ This command is part of a package not provided directly from
+ opensuse. It can be found under several places for suse.
+ There are two ways to install the package: either using a rpm
+ or using a repo.
+ In the second case these are the command to issue in order to 
+ install it:
+
+    zypper addrepo -f http://download.opensuse.org/repositories/home:/jblunck:/beagleboard/openSUSE_11.2
+    zypper install uboot-mkimage
+
+@@
+        return 1
+    fi
+    
+
 }
 
 function debian_regs
@@ -69,17 +125,18 @@ if [ "${APT}" ];then
 fi
 }
 
-detect_host
 
-if [ "${REDHAT}" ] ; then
-	redhat_software
-fi
-
-if [ "${SUSE}" ] ; then
-	suse_regs
-fi
-
-if [ "${DEBIAN}" ] ; then
-	debian_regs
-fi
+BUILD_HOST=${BUILD_HOST:="$( detect_host )"}
+info "Detected build host [$BUILD_HOST]"
+case "$BUILD_HOST" in
+    redhat*)
+	    redhat_reqs
+        ;;
+    debian*)
+	    debian_regs
+        ;;
+    suse*)
+	    suse_regs "$BUILD_HOST" || error "Failed dependency check"
+        ;;
+esac
 
