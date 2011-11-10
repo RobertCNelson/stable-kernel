fc09f16a48d20ae2de0a079e85a20bd9026317b4
Robert Nelson <robertcnelson@gmail.com>
2011-11-10 10:05:20 -0600

    scripts: and now use git trees over local patches
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -22,19 +22,7 @@ patch -s -p1 < "${DIR}/patches/trivial/0001-USB-ehci-use-packed-aligned-4-instea
 
 function cpufreq {
 echo "[git] omap-cpufreq"
-#git pull git://github.com/RobertCNelson/linux.git omap_cpufreq_v3.1-rc8
-
-patch -s -p1 < "${DIR}/patches/git-rcn/0035-cpufreq-OMAP-cleanup-for-multi-SoC-support-move-into.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0036-cpufreq-OMAP-Add-SMP-support-for-OMAP4.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0037-cpufreq-OMAP-Enable-all-CPUs-in-shared-policy-mask.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0038-cpufreq-OMAP-notify-even-with-bad-boot-frequency.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0039-cpufreq-OMAP-move-clk-name-decision-to-init.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0040-cpufreq-OMAP-deny-initialization-if-no-mpudev.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0041-cpufreq-OMAP-dont-support-freq_table.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0042-cpufreq-OMAP-only-supports-OPP-library.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0043-cpufreq-OMAP-put-clk-if-cpu_init-failed.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0044-cpufreq-OMAP-fix-freq_table-leak.patch"
-
+git pull git://github.com/RobertCNelson/linux.git omap_cpufreq_v3.1-rc8
 }
 
 function dss2_next {
@@ -78,35 +66,13 @@ echo "[git] Micrel KZ8851 patches for: zippy2"
 #original from:
 #ftp://www.micrel.com/ethernet/8851/beagle_zippy_patches.tar.gz 137 KB 04/10/2010 12:26:00 AM
 
-#git pull git://github.com/RobertCNelson/linux.git micrel_ks8851_v3.1-rc8
-
-patch -s -p1 < "${DIR}/patches/git-rcn/0016-ksz8851-eeprom-93cx6-add-drive_data.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0017-ksz8851-eeprom-93cx6-add-eeprom_93cx6_write.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0018-ksz8851-read_mac_addr.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0019-ksz8851-93cx6-eeprom-access.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0020-ksz8851-move-to-header.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0021-ksz8851-move-more-to-header.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0022-ksz8851-share-ks8851_tx_hdr-union.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0023-ksz8851-add-is_level_irq.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0024-ksz8851-turn-off-hardware-interrupt-druing-receive-p.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0025-ksz8851-make-sure-is-awake-before-writing-mac.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0026-ksz8851-add-mutex-lock-unlock-to-ks.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0027-ksz8851-add-ks8851_tx_check.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0028-ksz8851-move-ks8851_set_powermode.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0029-ks8851.h-it-helps-to-include-the-include-file.patch"
-
+git pull git://github.com/RobertCNelson/linux.git micrel_ks8851_v3.1-rc8
 }
 
 function beagle {
 echo "[git] Board Patches for: BeagleBoard"
 
-#git pull git://github.com/RobertCNelson/linux.git omap_beagle_expansion_v3.1-rc9
-
-patch -s -p1 < "${DIR}/patches/git-rcn/0030-expansion-add-buddy-param-for-expansionboard-names.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0031-expansion-add-mmc-regulator-and-ds1307-rtc.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0032-expansion-add-zippy.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0033-expansion-add-zippy2.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0034-expansion-add-trainer.patch"
+git pull git://github.com/RobertCNelson/linux.git omap_beagle_expansion_v3.1-rc9
 
 patch -s -p1 < "${DIR}/patches/beagle/ulcd/0001-WIP-ARM-OMAP-Beagle-ulcd.patch"
 
@@ -120,23 +86,7 @@ echo "[git] Board Patches for: igepv2"
 #pulled from: http://git.igep.es/?p=pub/scm/linux-omap-2.6.git;a=summary
 #git pull git://git.igep.es/pub/scm/linux-omap-2.6.git master
 
-#git pull git://github.com/RobertCNelson/linux.git omap_igepv_v3.1-rc7
-
-patch -s -p1 < "${DIR}/patches/git-rcn/0001-OMAP3-igep00x0-Introduce-common-functions-for-IGEP-b.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0002-OMAP3-igep0020-Remove-IGEP0030-support-from-board-ig.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0003-OMAP3-igep0020-Update-get-hardware-revision-function.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0004-OMAP3-igep0020-Use-common-flash-initialitzation-func.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0005-OMAP3-igep0020-Use-common-smsc911x-initialitzation-f.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0006-OMAP3-igep0020-Reduce-the-amount-of-code-for-gpio-le.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0007-OMAP3-igep0020-Use-common-wifi_bt-initialitzation-fu.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0008-OMAP3-igep0020-Use-common-twl4030-initialitzation-fu.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0009-OMAP3-igep0020-Use-common-display-initialitzation-fu.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0010-OMAP3-igep0020-Ensure-pins-are-mux-d.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0011-OMAP3-igep0020-Add-board.ei485-early-parameter.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0012-OMAP3-igep0020-Add-IGEP0022-expansion-board-support.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0013-OMAP3-igep0020-Fix-double-registration-of-omapdss-de.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0014-OMAP3-igep0020-Fix-mux-when-using-board.ei485-kernel.patch"
-patch -s -p1 < "${DIR}/patches/git-rcn/0015-board-igeo00x0-fix-igep00x0_tsc2046_init.patch"
+git pull git://github.com/RobertCNelson/linux.git omap_igepv_v3.1-rc7
 
 #Misha Manulis reply+i-2124203-89b49457cbc2980b8763661d...@reply.github.com
 #
