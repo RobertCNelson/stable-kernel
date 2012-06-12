e210af7c14b2a29bd1909f52bb5368b249bceeef
Robert Nelson <robertcnelson@gmail.com>
2012-06-12 11:16:27 -0500

    3.4.2-x0 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/push-n-tag-release.sh
+++ tools/push-n-tag-release.sh
@@ -28,24 +28,22 @@ DIR=$PWD
 BRANCH="v3.4.x"
 
 if [ -e ${DIR}/version.sh ]; then
+	unset RC_PATCH
+	unset TABLE_PATCH
 	. version.sh
 
 	if [ "${RC_PATCH}" ]; then
 		git commit -a -m "${RC_KERNEL}${RC_PATCH}-${BUILD} release" -s
 		git tag -a "${RC_KERNEL}${RC_PATCH}-${BUILD}" -m "${RC_KERNEL}${RC_PATCH}-${BUILD}"
-		git push origin ${BRANCH}
-		git push origin ${BRANCH} --tags
 	else if [ "${STABLE_PATCH}" ] ; then
 		git commit -a -m "${KERNEL_REL}.${STABLE_PATCH}-${BUILD} release" -s
 		git tag -a "${KERNEL_REL}.${STABLE_PATCH}-${BUILD}" -m "${KERNEL_REL}.${STABLE_PATCH}-${BUILD}"
-		git push origin ${BRANCH}
-		git push origin ${BRANCH} --tags
 	else
 		git commit -a -m "${KERNEL_REL}-${BUILD} release" -s
 		git tag -a "${KERNEL_REL}-${BUILD}" -m "${KERNEL_REL}-${BUILD}"
-		git push origin ${BRANCH}
-		git push origin ${BRANCH} --tags
 	fi
 	fi
+	git push origin ${BRANCH}
+	git push origin ${BRANCH} --tags
 fi
 
