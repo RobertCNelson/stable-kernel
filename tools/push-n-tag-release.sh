aba87a2693dfa35023d37fd4d5a97200520bd3c0
Robert Nelson <robertcnelson@gmail.com>
2012-08-01 09:33:26 -0500

    cleanup: nit picky things
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/push-n-tag-release.sh
+++ tools/push-n-tag-release.sh
@@ -22,22 +22,27 @@
 
 #yeah, i'm getting lazy..
 
-unset NO_DEVTMPS
-
 DIR=$PWD
+unset BRANCH
+#BRANCH="am33x-v3.2"
 
 if [ -e ${DIR}/version.sh ]; then
+	unset RC_PATCH
 	unset STABLE_PATCH
 	. version.sh
 
-	if [ "${STABLE_PATCH}" ] ; then
+	if [ "${RC_PATCH}" ] ; then
+		git commit -a -m "${RC_KERNEL}${RC_PATCH}-${BUILD} release" -s
+		git tag -a "${RC_KERNEL}${RC_PATCH}-${BUILD}" -m "${RC_KERNEL}${RC_PATCH}-${BUILD}"
+	else if [ "${STABLE_PATCH}" ] ; then
 		git commit -a -m "${KERNEL_REL}.${STABLE_PATCH}-${BUILD} release" -s
 		git tag -a "${KERNEL_REL}.${STABLE_PATCH}-${BUILD}" -m "${KERNEL_REL}.${STABLE_PATCH}-${BUILD}"
 	else
 		git commit -a -m "${KERNEL_REL}-${BUILD} release" -s
 		git tag -a "${KERNEL_REL}-${BUILD}" -m "${KERNEL_REL}-${BUILD}"
 	fi
-	git push origin
-	git push origin --tags
+	fi
+	git push origin ${BRANCH}
+	git push origin ${BRANCH} --tags
 fi
 
