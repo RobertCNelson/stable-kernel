284efa298fd0b5ea0c2326014e6d452045826130
Robert Nelson <robertcnelson@gmail.com>
2012-08-27 10:00:25 -0500

    cleanup: move gcc testing to seperate file
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/rebuild_deb.sh
+++ tools/rebuild_deb.sh
@@ -96,7 +96,7 @@ function make_dtbs_pkg {
 	cd ${DIR}/
 }
 
-  /bin/bash -e ${DIR}/tools/host_det.sh || { exit 1 ; }
+/bin/bash -e ${DIR}/tools/host_det.sh || { exit 1 ; }
 
 if [ -e ${DIR}/system.sh ] ; then
 	unset CC
@@ -105,17 +105,11 @@ if [ -e ${DIR}/system.sh ] ; then
 	unset LINUX_GIT
 	unset LOCAL_PATCH_DIR
 	source ${DIR}/system.sh
+	/bin/bash -e "${DIR}/scripts/gcc.sh" || { exit 1 ; }
 
 	source ${DIR}/version.sh
 	export LINUX_GIT
 	export LATEST_GIT
-
-	if [ "x${GCC_OVERRIDE}" != "x" ] ; then
-		GCC="${GCC_OVERRIDE}"
-	fi
-	echo ""
-	echo "Debug: using $(LC_ALL=C ${CC}gcc --version)"
-	echo ""
 
 	if [ "${LATEST_GIT}" ] ; then
 		echo ""
