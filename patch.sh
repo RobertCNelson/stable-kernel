006d27355e15d5be62134271a33ca2f16e62c703
Robert Nelson <robertcnelson@gmail.com>
2012-09-05 14:53:16 -0500

    devkit8000: drop, just push mainline if you want a real fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -89,11 +89,6 @@ sprz319_erratum () {
 	${git} "${DIR}/patches/sprz319-erratum-2.1/0001-Fix-sprz319-erratum-2.1.patch"
 }
 
-devkit8000 () {
-	echo "Board Patches for: devkit8000"
-	${git} "${DIR}/patches/devkit8000/0001-arm-omap-devkit8000-for-lcd-use-samsung_lte_panel.patch"
-}
-
 panda () {
 	echo "Board Patches for: PandaBoard"
 	#Status: not for upstream: push device tree version upstream...
@@ -172,7 +167,6 @@ beagle
 #disabled as it breaks beagle c4...
 #sprz319_erratum
 
-devkit8000
 panda
 omap_fixes
 drm
