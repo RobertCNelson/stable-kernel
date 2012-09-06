c0ea90e2f353f96596504d4f0ee0edcf0d5c9d07
Robert Nelson <robertcnelson@gmail.com>
2012-09-05 20:42:21 -0500

    script: merge in updates
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -22,7 +22,14 @@
 
 # Split out, so build_kernel.sh and build_deb.sh can share..
 
-# DIR=`pwd`
+git="git am"
+#git="git am --whitespace=fix"
+
+source ${DIR}/system.sh
+
+if [ "${RUN_BISECT}" ] ; then
+	git="git apply"
+fi
 
 echo "Starting patch.sh"
 
@@ -32,64 +39,63 @@ git_add () {
 }
 
 cleanup () {
-	git format-patch -8
+	git format-patch -2 -o ${DIR}/patches/
 	exit
 }
 
 distro () {
 	echo "Distro Specific Patches"
-	git am "${DIR}/patches/distro/0001-kbuild-deb-pkg-set-host-machine-after-dpkg-gencontro.patch"
+	${git} "${DIR}/patches/distro/0001-kbuild-deb-pkg-set-host-machine-after-dpkg-gencontro.patch"
 }
 
 sakoman () {
 	echo "Patches from: Sakoman git tree"
-	git am "${DIR}/patches/sakoman/0001-OMAP-DSS2-add-bootarg-for-selecting-svideo.patch"
-	git am "${DIR}/patches/sakoman/0002-video-add-timings-for-hd720.patch"
-	git am "${DIR}/patches/sakoman/0003-omap-mmc-Adjust-dto-to-eliminate-timeout-errors.patch"
+	${git} "${DIR}/patches/sakoman/0001-OMAP-DSS2-add-bootarg-for-selecting-svideo.patch"
+	${git} "${DIR}/patches/sakoman/0002-video-add-timings-for-hd720.patch"
+	${git} "${DIR}/patches/sakoman/0003-omap-mmc-Adjust-dto-to-eliminate-timeout-errors.patch"
 }
-
 
 beagle () {
 	echo "Board Patches for: BeagleBoard"
 
-	git am "${DIR}/patches/beagle/expansion/0001-Beagle-expansion-add-buddy-param-for-expansionboard-.patch"
-	git am "${DIR}/patches/beagle/expansion/0002-Beagle-expansion-add-zippy.patch"
-	git am "${DIR}/patches/beagle/expansion/0003-Beagle-expansion-add-zippy2.patch"
-	git am "${DIR}/patches/beagle/expansion/0004-Beagle-expansion-add-trainer.patch"
-	git am "${DIR}/patches/beagle/expansion/0005-Beagle-expansion-add-CircuitCo-ulcd-Support.patch"
-	git am "${DIR}/patches/beagle/expansion/0006-Beagle-expansion-add-wifi.patch"
-	git am "${DIR}/patches/beagle/expansion/0007-Beagle-expansion-add-beaglefpga.patch"
-	git am "${DIR}/patches/beagle/expansion/0008-Enable-buddy-spidev.patch"
+	${git} "${DIR}/patches/beagle/expansion/0001-Beagle-expansion-add-buddy-param-for-expansionboard-.patch"
+	${git} "${DIR}/patches/beagle/expansion/0002-Beagle-expansion-add-zippy.patch"
+	${git} "${DIR}/patches/beagle/expansion/0003-Beagle-expansion-add-zippy2.patch"
+	${git} "${DIR}/patches/beagle/expansion/0004-Beagle-expansion-add-trainer.patch"
+	${git} "${DIR}/patches/beagle/expansion/0005-Beagle-expansion-add-CircuitCo-ulcd-Support.patch"
+	${git} "${DIR}/patches/beagle/expansion/0006-Beagle-expansion-add-wifi.patch"
+	${git} "${DIR}/patches/beagle/expansion/0007-Beagle-expansion-add-beaglefpga.patch"
+	${git} "${DIR}/patches/beagle/expansion/0008-Enable-buddy-spidev.patch"
 
-	git am "${DIR}/patches/beagle/0001-beagleboard-reinstate-usage-of-hi-speed-PLL-divider.patch"
-	git am "${DIR}/patches/beagle/0002-Turn-on-the-USB-regulator-on-Beagle-xM-explicitly.patch"
-	git am "${DIR}/patches/beagle/0003-meego-modedb-add-Toshiba-LTA070B220F-800x480-support.patch"
-	git am "${DIR}/patches/beagle/0004-default-to-fifo-mode-5-for-old-musb-beagles.patch"
-	git am "${DIR}/patches/beagle/0005-omap3_beagle-init-uart2-for-beagle-rev-AX-BX-only.patch"
+	${git} "${DIR}/patches/beagle/0001-beagleboard-reinstate-usage-of-hi-speed-PLL-divider.patch"
+	${git} "${DIR}/patches/beagle/0002-Turn-on-the-USB-regulator-on-Beagle-xM-explicitly.patch"
+	${git} "${DIR}/patches/beagle/0003-meego-modedb-add-Toshiba-LTA070B220F-800x480-support.patch"
+	${git} "${DIR}/patches/beagle/0004-default-to-fifo-mode-5-for-old-musb-beagles.patch"
+	${git} "${DIR}/patches/beagle/0005-omap3_beagle-init-uart2-for-beagle-rev-AX-BX-only.patch"
 
-	git am "${DIR}/patches/beagle/0006-backlight-Add-TLC59108-backlight-control-driver.patch"
-	git am "${DIR}/patches/beagle/0007-tlc59108-adjust-for-beagleboard-uLCD7.patch"
-	git am "${DIR}/patches/beagle/0008-zeroMAP-Open-your-eyes.patch"
+	${git} "${DIR}/patches/beagle/0006-backlight-Add-TLC59108-backlight-control-driver.patch"
+	${git} "${DIR}/patches/beagle/0007-tlc59108-adjust-for-beagleboard-uLCD7.patch"
+	${git} "${DIR}/patches/beagle/0008-zeroMAP-Open-your-eyes.patch"
 }
 
 devkit8000 () {
 	echo "Board Patches for: devkit8000"
-	git am "${DIR}/patches/devkit8000/0001-arm-omap-devkit8000-for-lcd-use-samsung_lte_panel-2.6.37-git10.patch"
+	${git} "${DIR}/patches/devkit8000/0001-arm-omap-devkit8000-for-lcd-use-samsung_lte_panel-2.6.37-git10.patch"
 }
 
 panda () {
 	echo "Board Patches for: PandaBoard"
-	git am "${DIR}/patches/panda/0001-panda-fix-wl12xx-regulator.patch"
-	git am "${DIR}/patches/panda/0002-panda-enable-bluetooth.patch"
-	git am "${DIR}/patches/panda/0003-ti-st-st-kim-fixing-firmware-path.patch"
+	${git} "${DIR}/patches/panda/0001-panda-fix-wl12xx-regulator.patch"
+	${git} "${DIR}/patches/panda/0002-panda-enable-bluetooth.patch"
+	${git} "${DIR}/patches/panda/0003-ti-st-st-kim-fixing-firmware-path.patch"
 }
 
 omap_fixes () {
 	echo "omap cherry pick fixes"
-	git am "${DIR}/patches/omap_fixes/0001-omap3-Increase-limit-on-bootarg-mpurate.patch"
-	git am "${DIR}/patches/omap_fixes/0002-OMAP2-UART-Remove-cpu-checks-for-populating-errata-f.patch"
-	git am "${DIR}/patches/omap_fixes/0003-OMAP2-UART-enable-tx-wakeup-bit-for-wer-reg.patch"
-	git am "${DIR}/patches/omap_fixes/0004-OMAP2-UART-replace-omap34xx-omap4xx-cpu-checks-with-.patch"
+	${git} "${DIR}/patches/omap_fixes/0001-omap3-Increase-limit-on-bootarg-mpurate.patch"
+	${git} "${DIR}/patches/omap_fixes/0002-OMAP2-UART-Remove-cpu-checks-for-populating-errata-f.patch"
+	${git} "${DIR}/patches/omap_fixes/0003-OMAP2-UART-enable-tx-wakeup-bit-for-wer-reg.patch"
+	${git} "${DIR}/patches/omap_fixes/0004-OMAP2-UART-replace-omap34xx-omap4xx-cpu-checks-with-.patch"
 	#in 3.4.9
 	#git am "${DIR}/patches/omap_fixes/0005-Unconditional-call-to-smp_cross_call-on-UP-crashes.patch"
 }
@@ -99,45 +105,45 @@ omapdrm () {
 
 	echo "Patches for 3.4-rc1-cma-v24"
 	#git://git.linaro.org/people/mszyprowski/linux-dma-mapping.git 3.4-rc1-cma-v24
-	git am "${DIR}/patches/drm/cma/0001-mm-page_alloc-remove-trailing-whitespace.patch"
-	git am "${DIR}/patches/drm/cma/0002-mm-compaction-introduce-isolate_migratepages_range.patch"
-	git am "${DIR}/patches/drm/cma/0003-mm-compaction-introduce-map_pages.patch"
-	git am "${DIR}/patches/drm/cma/0004-mm-compaction-introduce-isolate_freepages_range.patch"
-	git am "${DIR}/patches/drm/cma/0005-mm-compaction-export-some-of-the-functions.patch"
-	git am "${DIR}/patches/drm/cma/0006-mm-page_alloc-introduce-alloc_contig_range.patch"
-	git am "${DIR}/patches/drm/cma/0007-mm-page_alloc-change-fallbacks-array-handling.patch"
-	git am "${DIR}/patches/drm/cma/0008-mm-mmzone-MIGRATE_CMA-migration-type-added.patch"
-	git am "${DIR}/patches/drm/cma/0009-mm-page_isolation-MIGRATE_CMA-isolation-functions-ad.patch"
-	git am "${DIR}/patches/drm/cma/0010-mm-Serialize-access-to-min_free_kbytes.patch"
-	git am "${DIR}/patches/drm/cma/0011-mm-extract-reclaim-code-from-__alloc_pages_direct_re.patch"
-	git am "${DIR}/patches/drm/cma/0012-mm-trigger-page-reclaim-in-alloc_contig_range-to-sta.patch"
-	git am "${DIR}/patches/drm/cma/0013-drivers-add-Contiguous-Memory-Allocator.patch"
-	git am "${DIR}/patches/drm/cma/0014-X86-integrate-CMA-with-DMA-mapping-subsystem.patch"
-	git am "${DIR}/patches/drm/cma/0015-ARM-integrate-CMA-with-DMA-mapping-subsystem.patch"
+	${git} "${DIR}/patches/drm/cma/0001-mm-page_alloc-remove-trailing-whitespace.patch"
+	${git} "${DIR}/patches/drm/cma/0002-mm-compaction-introduce-isolate_migratepages_range.patch"
+	${git} "${DIR}/patches/drm/cma/0003-mm-compaction-introduce-map_pages.patch"
+	${git} "${DIR}/patches/drm/cma/0004-mm-compaction-introduce-isolate_freepages_range.patch"
+	${git} "${DIR}/patches/drm/cma/0005-mm-compaction-export-some-of-the-functions.patch"
+	${git} "${DIR}/patches/drm/cma/0006-mm-page_alloc-introduce-alloc_contig_range.patch"
+	${git} "${DIR}/patches/drm/cma/0007-mm-page_alloc-change-fallbacks-array-handling.patch"
+	${git} "${DIR}/patches/drm/cma/0008-mm-mmzone-MIGRATE_CMA-migration-type-added.patch"
+	${git} "${DIR}/patches/drm/cma/0009-mm-page_isolation-MIGRATE_CMA-isolation-functions-ad.patch"
+	${git} "${DIR}/patches/drm/cma/0010-mm-Serialize-access-to-min_free_kbytes.patch"
+	${git} "${DIR}/patches/drm/cma/0011-mm-extract-reclaim-code-from-__alloc_pages_direct_re.patch"
+	${git} "${DIR}/patches/drm/cma/0012-mm-trigger-page-reclaim-in-alloc_contig_range-to-sta.patch"
+	${git} "${DIR}/patches/drm/cma/0013-drivers-add-Contiguous-Memory-Allocator.patch"
+	${git} "${DIR}/patches/drm/cma/0014-X86-integrate-CMA-with-DMA-mapping-subsystem.patch"
+	${git} "${DIR}/patches/drm/cma/0015-ARM-integrate-CMA-with-DMA-mapping-subsystem.patch"
 
 	#posted: 13 Mar 2012 for 3.4
-	git am "${DIR}/patches/drm/0001-omap2-add-drm-device.patch"
+	${git} "${DIR}/patches/drm/0001-omap2-add-drm-device.patch"
 
 	#might be merged in 3.4
-	git am "${DIR}/patches/drm/0002-ARM-OMAP2-3-HWMOD-Add-missing-flags-for-dispc-class.patch"
-	git am "${DIR}/patches/drm/0003-ARM-OMAP2-3-HWMOD-Add-missing-flag-for-rfbi-class.patch"
-	git am "${DIR}/patches/drm/0004-ARM-OMAP3-HWMOD-Add-omap_hwmod_class_sysconfig-for-d.patch"
+	${git} "${DIR}/patches/drm/0002-ARM-OMAP2-3-HWMOD-Add-missing-flags-for-dispc-class.patch"
+	${git} "${DIR}/patches/drm/0003-ARM-OMAP2-3-HWMOD-Add-missing-flag-for-rfbi-class.patch"
+	${git} "${DIR}/patches/drm/0004-ARM-OMAP3-HWMOD-Add-omap_hwmod_class_sysconfig-for-d.patch"
 }
 
 dsp () {
 	echo "dsp patches"
-	git am "${DIR}/patches/dsp/0001-OMAP2-control-new-APIs-to-configure-boot-address-and.patch"
-	git am "${DIR}/patches/dsp/0002-OMAP-dsp-interface-to-control-module-functions.patch"
-	git am "${DIR}/patches/dsp/0003-staging-tidspbridge-use-scm-functions-to-set-boot-ad.patch"
+	${git} "${DIR}/patches/dsp/0001-OMAP2-control-new-APIs-to-configure-boot-address-and.patch"
+	${git} "${DIR}/patches/dsp/0002-OMAP-dsp-interface-to-control-module-functions.patch"
+	${git} "${DIR}/patches/dsp/0003-staging-tidspbridge-use-scm-functions-to-set-boot-ad.patch"
 }
 
 sgx_mainline () {
 	echo "patches needed for external sgx bins"
-	git am "${DIR}/patches/sgx/0001-Revert-drm-kill-drm_sman.patch"
+	${git} "${DIR}/patches/sgx/0001-Revert-drm-kill-drm_sman.patch"
 }
 
 led () {
-	git am "${DIR}/patches/led/0001-leds-heartbeat-stop-on-shutdown-reboot-or-panic.patch"
+	${git} "${DIR}/patches/led/0001-leds-heartbeat-stop-on-shutdown-reboot-or-panic.patch"
 }
 
 sgx () {
