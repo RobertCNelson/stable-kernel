d6b0c7ee08a13769a5b8e3efd1257ffee089042d
Robert Nelson <robertcnelson@gmail.com>
2012-10-08 14:11:11 -0500

    merge all script changes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -1,7 +1,49 @@
 #!/bin/bash
+#
+# Copyright (c) 2009-2012 Robert Nelson <robertcnelson@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 # Split out, so build_kernel.sh and build_deb.sh can share..
 
+git="git am"
+#git="git am --whitespace=fix"
+
+if [ -f ${DIR}/system.sh ] ; then
+	source ${DIR}/system.sh
+fi
+
+if [ "${RUN_BISECT}" ] ; then
+	git="git apply"
+fi
+
 echo "Starting patch.sh"
+
+git_add () {
+	git add .
+	git commit -a -m 'testing patchset'
+}
+
+cleanup () {
+	git format-patch -${number} -o ${DIR}/patches/
+	exit
+}
 
 function bugs_trivial {
 echo "bugs and trivial stuff"
