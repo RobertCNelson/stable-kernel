6364c466e9b4767a50195e5e2c029f0d650e926d
Robert Nelson <robertcnelson@gmail.com>
2012-12-03 16:53:31 -0600

    3.7-rc8-x1 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -23,8 +23,8 @@ config="omap2plus_defconfig"
 
 #Kernel/Build
 KERNEL_REL=3.7
-KERNEL_TAG=${KERNEL_REL}-rc7
-BUILD=x0.3
+KERNEL_TAG=${KERNEL_REL}-rc8
+BUILD=x1
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
