ca29ee19c0c065d9df2212ea2d01fd194855fd44
Robert Nelson <robertcnelson@gmail.com>
2012-12-06 12:37:07 -0600

    merge with unified kernel
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -93,6 +93,7 @@ am33x () {
 
 	echo "dir: cpufreq"
 	${git} "${DIR}/patches/cpufreq/0001-arm-dts-AM33XX-Add-device-tree-OPP-table.patch"
+	${git} "${DIR}/patches/cpufreq/0002-am33xx-DT-add-commented-out-OPP-values-for-ES2.0.patch"
 
 	echo "dir: adc"
 	${git} "${DIR}/patches/adc/0001-input-TSC-ti_tscadc-Correct-register-usage.patch"
@@ -236,6 +237,11 @@ am33x () {
 	${git} "${DIR}/patches/hdmi/0005-am335x-bonelt-dts-Add-DT-node-to-probe-NXP-driver.patch"
 	${git} "${DIR}/patches/hdmi/0006-Add-capebus-override-and-pinmux-for-da8xx-dt.patch"
 	${git} "${DIR}/patches/hdmi/0007-nxp-Makefile-Undefine-TDA_CEC-we-don-t-use-CEC-featu.patch"
+
+	echo "dir: pruss"
+	${git} "${DIR}/patches/pruss/0001-uio-uio_pruss-port-to-AM33xx.patch"
+	${git} "${DIR}/patches/pruss/0002-ARM-omap-add-DT-support-for-deasserting-hardware-res.patch"
+	${git} "${DIR}/patches/pruss/0003-ARM-dts-AM33xx-PRUSS-support.patch"
 }
 
 arm () {
