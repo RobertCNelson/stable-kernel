ac90bc60d1dde713c5b42965751de70d860b5682
Robert Nelson <robertcnelson@gmail.com>
2013-03-10 21:47:56 -0500

    deb: make sure bc is installed
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/host_det.sh
+++ tools/host_det.sh
@@ -93,6 +93,7 @@ Missing mkimage command.
 function debian_regs
 {
 	unset deb_pkgs
+	dpkg -l | grep bc >/dev/null || deb_pkgs+="bc "
 	dpkg -l | grep build-essential >/dev/null || deb_pkgs+="build-essential "
 	dpkg -l | grep device-tree-compiler >/dev/null || deb_pkgs+="device-tree-compiler "
 	dpkg -l | grep lsb-release >/dev/null || deb_pkgs+="lsb-release "
