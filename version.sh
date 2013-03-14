7075b8d494d5fa33d8f940d56b11420d07d125e0
Robert Nelson <robertcnelson@gmail.com>
2013-03-14 15:10:54 -0500

    sync with beaglebone tree
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -21,8 +21,8 @@ config="omap2plus_defconfig"
 
 #Kernel/Build
 KERNEL_REL=3.8
-KERNEL_TAG=${KERNEL_REL}.2
-BUILD=x0.2
+KERNEL_TAG=${KERNEL_REL}.3
+BUILD=x0.3
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
