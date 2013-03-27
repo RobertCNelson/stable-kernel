f12e3bcfe6cca8f2f2aa57806c42a0f518e6fd17
Robert Nelson <robertcnelson@gmail.com>
2013-03-27 17:39:21 -0500

    beagle/panda: initial v3.9.x tree
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -20,15 +20,16 @@ fi
 config="omap2plus_defconfig"
 
 #Kernel/Build
-KERNEL_REL=3.8
-KERNEL_TAG=${KERNEL_REL}.4
-BUILD=x2
+KERNEL_REL=3.9
+KERNEL_TAG=${KERNEL_REL}-rc4
+#BUILD=x2
+BUILD=d0
 
 #v3.X-rcX + upto SHA
-#KERNEL_SHA=""
+KERNEL_SHA="f8966048c128eff4f57f7c9e7f71360ebd10e1a9"
 
 #git branch
-BRANCH="v3.8.x"
+BRANCH="v3.9.x"
 
 BUILDREV=1.0
 DISTRO=cross
