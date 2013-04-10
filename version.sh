e6ce1ddb853fb9282a6fedf7a5abe7f0e6c11db6
Robert Nelson <robertcnelson@gmail.com>
2013-04-09 22:05:30 -0500

    config: omap4: enable CONFIG_ARM_ERRATA_775420 (set by default in v3.10/next
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -22,7 +22,7 @@ config="omap2plus_defconfig"
 #Kernel/Build
 KERNEL_REL=3.9
 KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=x0.1
+BUILD=x0.2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA="118c9a45fdacc6fe57910fa1d048e2d5bbc193f4"
