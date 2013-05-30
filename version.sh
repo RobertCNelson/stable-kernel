de24f0ff85a198284d215bd09cb59a8ef9b06604
Robert Nelson <robertcnelson@gmail.com>
2013-05-30 11:24:08 -0500

    bump: v3.8.13
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -21,8 +21,8 @@ linaro_toolchain="cortex_gcc_4_7"
 
 #Kernel/Build
 KERNEL_REL=3.8
-KERNEL_TAG=${KERNEL_REL}.11
-BUILD=x3.4
+KERNEL_TAG=${KERNEL_REL}.13
+BUILD=x3.5
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
