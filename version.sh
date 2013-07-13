056420fd1297703bb2bdf36d545f0794f4ba19c8
Robert Nelson <robertcnelson@gmail.com>
2013-07-13 15:04:21 -0500

    3.10.1-x3 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -22,8 +22,8 @@ linaro_toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.10
-KERNEL_TAG=${KERNEL_REL}
-BUILD=x2.3
+KERNEL_TAG=${KERNEL_REL}.1
+BUILD=x3
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
