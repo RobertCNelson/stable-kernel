8dae208330c09a83c2d0d64a6e7d9ee244db69a6
Robert Nelson <robertcnelson@gmail.com>
2015-04-15 11:45:19 -0500

    just use ccache
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/rebuild_deb.sh
+++ tools/rebuild_deb.sh
@@ -158,12 +158,11 @@ unset LINUX_GIT
 . ${DIR}/system.sh
 /bin/sh -e "${DIR}/scripts/gcc.sh" || { exit 1 ; }
 . ${DIR}/.CC
-if [ ${AUTO_BUILD} ] ; then
-	if [ -f /usr/bin/ccache ] ; then
-		CC="ccache ${CC}"
-	fi
-fi
 echo "CROSS_COMPILE=${CC}"
+if [ -f /usr/bin/ccache ] ; then
+	echo "ccache [enabled]"
+	CC="ccache ${CC}"
+fi
 
 . ${DIR}/version.sh
 export LINUX_GIT
