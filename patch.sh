ee5437428930380b3c857227bcef73ae67a99ab9
Robert Nelson <robertcnelson@gmail.com>
2018-01-10 11:54:34 -0600

    patch: use 4.14.rt example
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -197,10 +197,10 @@ rt () {
 
 		cd ../
 		if [ ! -d ./linux-rt-devel ] ; then
-			${git_bin} clone -b linux-4.9.y-rt-patches https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git --depth=1
+			${git_bin} clone -b linux-4.14.y-rt-patches https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git --depth=1
 		else
 			rm -rf ./linux-rt-devel || true
-			${git_bin} clone -b linux-4.9.y-rt-patches https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git --depth=1
+			${git_bin} clone -b linux-4.14.y-rt-patches https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git --depth=1
 		fi
 
 		cd ./KERNEL/
